eb1bb8f8faad994899b8e923f83b279251ffc42d
Lorna Lynch <lornalynchit@yahoo.com>
2020-03-05 18:38:36 -0500

    search bar

---
+++ models/product.js
@@ -0,0 +1,48 @@
+/*
+productName,
+productDesc, 
+productPrice, 
+productCategory, 
+productQuantity, 
+productImage
+*/
+
+module.exports = function(sequelize, DataTypes) {
+    const Product = sequelize.define("Product", {
+      // Giving the Product model a name of type STRING
+      productName: {
+          type: DataTypes.STRING,
+          allowNull: false
+      },
+      productImage: {
+          type: DataTypes.STRING,
+          allowNull: true
+      },
+      productDesc: {
+          type: DataTypes.TEXT,
+          allowNull: false
+      },
+      productPrice: {
+          type: DataTypes.DECIMAL(10, 2),
+          allowNull: false
+      },
+      productCategory: {
+          type: DataTypes.STRING,
+          allowNull: false
+      },
+      productQuantity: {
+          type: DataTypes.INTEGER,
+          allowNull: false
+      }
+    });
+  
+    Product.associate = function(models) {
+      // Associating Product with Posts
+      // When an Product is deleted, also delete any associated Posts
+      Product.hasMany(models.Post, {
+        onDelete: "cascade"
+      });
+    };
+  
+    return Product;
+  };
