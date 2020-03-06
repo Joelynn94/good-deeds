3e7b0e78454bb130d9b6c258eb01f5e5d0f119ea
Joe Lynn <josephlynn94@gmail.com>
2020-03-05 19:02:33 -0500

    changed seeds file

--- db/seed.sql
+++ db/seed.sql
@@ -1,8 +1,9 @@
+use goodDeeds_DB;
 INSERT INTO products
-(productName, productImage, productDesc, productPrice, productCategory, productQuantity)
+(productName, productImage, productDesc, productPrice, productCategory, productQuantity, productInCart)
 VALUES
-('Coat', 'coat.jpeg', 'Winter Coat', 85.99, 'Winter Clothes', 1),
-('Backpack', 'backpack.jpeg', '15" Laptop Pocket', 15.99, 'School Supplies', 1),
-('Nike', 'shoe.jpeg', 'High Tops', 75.99, 'Shoes', 1),
-('Toothbrush', 'tootbrush.jpeg', 'Colgate', 4.99, 'Personal Hygiene', 1),
-('Suit', 'professional-clothes.jpeg', 'Olive Colored Jacket w/ Pants', 99.99, 'Professional Outfits', 1)
+('Coat', 'coat.jpeg', 'Winter Coat', 85.99, 'Winter Clothes', 1, false),
+('Backpack', 'backpack.jpeg', '15" Laptop Pocket', 15.99, 'School Supplies', 1, false),
+('Nike', 'shoe.jpeg', 'High Tops', 75.99, 'Shoes', 1, false),
+('Toothbrush', 'tootbrush.jpeg', 'Colgate', 4.99, 'Personal Hygiene', 1, false),
+('Suit', 'professional-clothes.jpeg', 'Olive Colored Jacket w/ Pants', 99.99, 'Professional Outfits', 1, false)
