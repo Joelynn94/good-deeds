ffdddfe6b3d964c87ee15c9b3fef6baafdd7e435
John Fields <jjfields24@outlook.com>
2020-03-05 19:57:13 -0500

    wip post to cart

--- public/js/cart.js
+++ public/js/cart.js
@@ -1,9 +1,11 @@
+'use strict'
 const addToCart = $(".card-cart")
 
 addToCart.on("click", function(){
   addToCart.map((cart) => {
-    let id = cart.dataValues.id;
+    let id = cart.id;
     console.log(id)
   })
+  
 })
 
