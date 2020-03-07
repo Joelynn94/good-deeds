d0cff53939c1faada80d45a5f7963200acecb014
John Fields <jjfields24@outlook.com>
2020-03-07 00:20:19 -0500

    row totals

--- public/js/cart.js
+++ public/js/cart.js
@@ -34,3 +34,22 @@ $('.container').on("click", '.remove-item', function(e) {
   removeFromCart($(this).data('id'))
   $('#removeDiv').remove();
 })
+
+function productTotal() {
+ // var cartItemContainer = document.getElementsByClassName('product-row')[0]
+  var cartRows = document.getElementsByClassName('product-inner')
+  var total = 0
+  for (var i = 0; i < cartRows.length; i++) {
+      var cartRow = cartRows[i]
+      var priceElement = cartRow.getElementsByClassName('row-price')[0]
+      var quantityElement = cartRow.getElementsByClassName('row-quantity')[0]
+      var price = parseFloat(priceElement.innerText.replace('$', ''))
+      var quantity = quantityElement.value
+      total = parseFloat(price) //price * quantity
+      document.getElementsByClassName('row-total')[i].innerText = '$' + total
+      //console.log(total)
+  }
+      //total = Math.round(total * 100) / 100
+      
+}
+productTotal(/*$(this).data('id')*/)
