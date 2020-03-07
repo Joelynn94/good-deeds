0aefd14d9b6d04e02624e33d284cca736cd34c5c
Joe Lynn <josephlynn94@gmail.com>
2020-03-06 23:15:56 -0500

    fixed typo in seeds, changed image file name

--- db/seed.sql
+++ db/seed.sql
@@ -5,5 +5,5 @@ VALUES
 ('Coat', 'img/coat.jpeg', 'Winter Coat', 85.99, 'Winter Clothes', 1, false),
 ('Backpack', 'img/backpack.jpeg', '15" Laptop Pocket', 15.99, 'School Supplies', 1, false),
 ('Nike', 'img/shoe.jpeg', 'High Tops', 75.99, 'Shoes', 1, false),
-('Toothbrush', 'img/tootbrush.jpeg', 'Colgate', 4.99, 'Personal Hygiene', 1, false),
+('Toothbrush', 'img/toothbrush.jpeg', 'Colgate', 4.99, 'Personal Hygiene', 1, false),
 ('Suit', 'img/professional-clothes.jpeg', 'Olive Colored Jacket w/ Pants', 99.99, 'Professional Outfits', 1, false)
